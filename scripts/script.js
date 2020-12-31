a5b237fd15f192337e74d2b96b781945f68c8ecf
ajcudd <ajcuddeback@gmail.com>
2020-12-31 14:37:49 -0500

    fixed pathing issue

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,14 @@
 let index = 0;
 const adoptableCarousel = document.querySelector('.adoptable-carousel');
 const mainCarouselImages = [
-    '../images/home-carousel/hc-0.jpg',
-    '../images/home-carousel/hc-1.jpg',
-    '../images/home-carousel/hc-2.jpg',
-    '../images/home-carousel/hc-3.jpg',
-    '../images/home-carousel/hc-4.jpg',
-    '../images/home-carousel/hc-5.jpg',
-    '../images/home-carousel/hc-6.jpg',
-    '../images/home-carousel/hc-7.jpg'
+    './images/home-carousel/hc-0.jpg',
+    './images/home-carousel/hc-1.jpg',
+    './images/home-carousel/hc-2.jpg',
+    './images/home-carousel/hc-3.jpg',
+    './images/home-carousel/hc-4.jpg',
+    './images/home-carousel/hc-5.jpg',
+    './images/home-carousel/hc-6.jpg',
+    './images/home-carousel/hc-7.jpg'
 ];
 const adoptablesAnimals = [
     '../images/reptile-adoptables/reptile-adopt-0.jpg',
@@ -20,7 +20,7 @@ const adoptablesAnimals = [
     '../images/mamals-adopt.png',
     '../images/reptiles-adopt.png',
     '../images/birds-adoptables/bird-adopt-1.jpeg',
-    '../images/featured-ambassador.png'   
+    '../images/featured-ambassador.png'
 ];
 let mainCarouselCounter = 0;
 const carouselSlide = function () {
@@ -43,51 +43,51 @@ let adopt1 = document.getElementById('adopt1');
 let adopt2 = document.getElementById('adopt2');
 let adopt3 = document.getElementById('adopt3');
 
-prevBtn.addEventListener('click', ()=>{
-    if (index<adoptablesAnimals.length-3){
-       
+prevBtn.addEventListener('click', () => {
+    if (index < adoptablesAnimals.length - 3) {
+
         adopt1.setAttribute('src', adoptablesAnimals[index])
-        adopt2.setAttribute('src',adoptablesAnimals[index+1])
-        adopt3.setAttribute('src',adoptablesAnimals[index+2])
-       
+        adopt2.setAttribute('src', adoptablesAnimals[index + 1])
+        adopt3.setAttribute('src', adoptablesAnimals[index + 2])
+
         console.log(adopt1)
         console.log(adopt2)
         console.log(adopt3)
-        
+
         index++;
 
-    }else{
-        index=0;   
+    } else {
+        index = 0;
         adopt1.setAttribute('src', adoptablesAnimals[index])
-        adopt2.setAttribute('src',adoptablesAnimals[index+1])
-        adopt3.setAttribute('src',adoptablesAnimals[index+2])
-          
+        adopt2.setAttribute('src', adoptablesAnimals[index + 1])
+        adopt3.setAttribute('src', adoptablesAnimals[index + 2])
+
     }
 })
 
-nextBtn.addEventListener('click', ()=>{
-    if (index<adoptablesAnimals.length-3){
-       
-        adopt1.setAttribute('src', adoptablesAnimals[index+2])
-        adopt2.setAttribute('src',adoptablesAnimals[index+1])
-        adopt3.setAttribute('src',adoptablesAnimals[index])
-      
+nextBtn.addEventListener('click', () => {
+    if (index < adoptablesAnimals.length - 3) {
+
+        adopt1.setAttribute('src', adoptablesAnimals[index + 2])
+        adopt2.setAttribute('src', adoptablesAnimals[index + 1])
+        adopt3.setAttribute('src', adoptablesAnimals[index])
+
         console.log(adopt1)
         console.log(adopt2)
         console.log(adopt3)
-        
+
         index++;
 
-    }else{
-        index=0;   
-        adopt1.setAttribute('src', adoptablesAnimals[index +2])
-        adopt2.setAttribute('src',adoptablesAnimals[index+1])
-        adopt3.setAttribute('src',adoptablesAnimals[index])
-        
+    } else {
+        index = 0;
+        adopt1.setAttribute('src', adoptablesAnimals[index + 2])
+        adopt2.setAttribute('src', adoptablesAnimals[index + 1])
+        adopt3.setAttribute('src', adoptablesAnimals[index])
+
     }
 
-// const adoptableWidth = adoptableCarousel.clientWidth;
-// adoptableCarousel.style.transform = `translateX(${-adoptableWidth/5}px)`;
+    // const adoptableWidth = adoptableCarousel.clientWidth;
+    // adoptableCarousel.style.transform = `translateX(${-adoptableWidth/5}px)`;
 })
 
 
