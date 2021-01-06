7445c15386d49415ab4c57dbed1a02b1e9f5b87f
ajcudd <ajcuddeback@gmail.com>
2021-01-05 21:39:33 -0500

    Added env port

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const app = express();
 
+const PORT = process.env.PORT || 3000;
+
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
-app.listen(3000)
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`)
+})
