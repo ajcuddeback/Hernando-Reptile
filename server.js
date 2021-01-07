2124971c61a0a2481ff800f7e354b3d6a851657d
ajcudd <ajcuddeback@gmail.com>
2021-01-07 11:49:02 -0500

    fixing port

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.post("/create-payment-intent", async (req, res) => {
     });
 });
 
-const PORT = 80;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}`)
