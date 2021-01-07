ff9582ebdde3c7bc25d3fe227d495840f34ca096
ajcudd <ajcuddeback@gmail.com>
2021-01-07 13:41:11 -0500

    added paramater that will not allow the user to enter an empty value

--- public/scripts/donation-form.js
+++ public/scripts/donation-form.js
@@ -1,7 +1,13 @@
 const donationForm = document.querySelector(".donation-form");
 let price;
-const handleDonationForm = () => {
+const handleDonationForm = event => {
+    if (!document.querySelector('[name="donation-amount"]').value) {
+        alert("Donation must not be empty!");
+        event.preventDefault();
+        return;
+    }
     price = document.querySelector('[name="donation-amount"]').value;
     localStorage.setItem('price', JSON.stringify(price));
 }
+
 donationForm.addEventListener('submit', handleDonationForm);
