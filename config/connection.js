ff2efab5e8dc5b0705ae81f10e46638e5df10be8
ajcudd <ajcuddeback@gmail.com>
2021-03-15 12:14:00 -0400

    adding dialect

--- config/connection.js
+++ config/connection.js
@@ -7,6 +7,7 @@ let sequelize;
 
 sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
     host: 'adoptables-do-user-8535508-0.b.db.ondigitalocean.com',
+    dialect: 'mysql',
     port: 25060
 })
 // } else {
