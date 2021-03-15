ffe635ff35b14368a12ea1dfa3583672f15ad995
ajcudd <ajcuddeback@gmail.com>
2021-03-15 12:18:47 -0400

    fixing

--- config/connection.js
+++ config/connection.js
@@ -18,5 +18,14 @@ sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.
 //     });
 // }
 
+sequelize
+    .authenticate()
+    .then(() => {
+        console.log('Connection has been established successfully.');
+    })
+    .catch((err) => {
+        console.log('Unable to connect to the database:', err);
+    });
+
 
 module.exports = sequelize;
